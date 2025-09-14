6893b1161f682c226a6ca8b0691eed307d01108a
DeRasser <derasser08@gmail.com>
2025-09-14 21:47:36 +0400

    Correction of some nuances

--- basecalculator.py
+++ basecalculator.py
@@ -1,11 +1,11 @@
-x = int(input('Enter your number: '))
+alif = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+x = input('Enter your number: ')
 base = int(input('Enter base of your number: '))
 for i in str(x):
-        if int(i) >= base:
+        if int(alif.index(i)) >= base:
             print("The number doesn't match your base!")
             exit()
 tobase = int(input('Enter base you want to convert your number to(max36): '))
-alif = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 dig = 0
 res = ''
 internum = 0
@@ -18,7 +18,7 @@ if base == 10:
     print(res)
 else:
     for i in range(len(str(x))):
-        internum = internum + int(str(x)[::-1][i]) * base ** i
+        internum = internum + alif.index(str(x)[::-1][i]) * base ** i
     while internum != 0:
         dig = internum % tobase
         res = alif[dig] + res
